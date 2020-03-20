d2411324fd6adb17e947abf34d8a54a7a6b9b6c9
zhangyangfei <674557629@qq.com>
2020-03-20 11:22:22 +0800

    poi

---
+++ report-poi/src/main/java/com/zyf/reportpoi/ReportPoiApplication.java
@@ -0,0 +1,13 @@
+package com.zyf.reportpoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportPoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportPoiApplication.class, args);
+	}
+
+}
---
+++ report-poi/src/test/java/com/zyf/reportpoi/ReportPoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyf.reportpoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportPoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
